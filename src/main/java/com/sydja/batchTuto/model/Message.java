a1a194b3bb328349b729a53abcc0ea3a1a45b18a
Djonko <prevostdjonko@gmail.com>
2015-12-11 01:40:51 +0000

    first commit

---
+++ src/main/java/com/sydja/batchTuto/model/Message.java
@@ -0,0 +1,45 @@
+package com.sydja.batchTuto.model;
+
+public class Message {
+
+	
+	private int id;
+	private String statut;
+	private String libelle;
+	private String content;
+	private String email;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getStatut() {
+		return statut;
+	}
+	public void setStatut(String statut) {
+		this.statut = statut;
+	}
+	public String getLibelle() {
+		return libelle;
+	}
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
